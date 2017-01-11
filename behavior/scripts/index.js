d4df036c29d3963f5ca5cd32d3229e740df7706f
Darryn Mitussis <darryn@imax.mitussis.net>
2017-01-11 09:24:32 +0000

    copy data from v1

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,49 +1,239 @@
-'use strict'
+var request = require('request');
+var util = require('util');
 
-exports.handle = (client) => {
-  // Create steps
-  const sayHello = client.createStep({
-    satisfied() {
-      return Boolean(client.getConversationState().helloSent)
-    },
 
-    prompt() {
-      client.addResponse('welcome')
-      client.addResponse('provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
+var smoochAPI = {
+	headers: {
+		'app-token': '9oe7vz42bik4rymee8cd8zgzq'
+	}
+};
 
-      client.updateConversationState({
-        helloSent: true
-      })
+// 'use strict';
+exports.handle = function (client) {
 
-      client.done()
-    }
-  })
 
-  const untrained = client.createStep({
-    satisfied() {
-      return false
-    },
 
-    prompt() {
-      client.addResponse('apology/untrained')
-      client.done()
-    }
-  })
 
-  client.runFlow({
-    classifications: {
-      // map inbound message classifications to names of streams
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
-    streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained],
-    },
-  })
-}
+
+
+	// Create steps
+	var sayHello = client.createStep({
+		satisfied: function () {
+			return Boolean(client.getConversationState().helloSent);
+		},
+
+		prompt: function () {
+			client.addResponse('welcome');
+			client.addResponse('provide/documentation', {
+				documentation_link: 'http://docs.init.ai',
+			});
+			client.addResponse('provide/instructions');
+
+			client.updateConversationState({
+				helloSent: true
+			});
+
+			client.done();
+		}
+	});
+
+	var untrained = client.createStep({
+		satisfied: function () {
+			return false;
+		},
+
+		prompt: function () {
+			client.addResponse('apology/untrained');
+			client.done();
+		}
+	});
+
+
+
+	// was collect city
+	var collectRole = client.createStep({
+
+
+		satisfied: function () {
+
+			console.log("collectRole / satisfied");
+
+
+			return Boolean(client.getConversationState().requstedRole);
+		},
+
+		extractInfo: function () {
+
+
+			var messagePart = client.getMessagePart();
+
+			console.log("init.ai");
+			console.log(JSON.stringify(messagePart));
+
+
+			var initId = messagePart.sender.id;
+			var smoochId = messagePart.sender.remote_id;
+
+			client.addTextResponse(initId + " | " + smoochId);
+
+
+			smoochAPI.url = 'https://api.smooch.io/v1/appusers/' + smoochId;
+
+			request.get(smoochAPI, function (error, response, body) {
+
+				console.log("smooch.io error:");
+				console.log(error);
+				// console.log(response);
+				// console.log(util.inspect(JSON.parse(body), false, null));
+				console.log("smooch.io:");
+				body = JSON.parse(body);
+
+				console.log(body);
+
+				var forename = body.appUser.givenName;
+				var surname = body.appUser.surname;
+				var client_id = body.appUser.userId;
+
+				client.updateUser(initId, 'first_name', forename);
+
+				client.updateUser(initId, 'last_name', surname);
+
+				client.updateUser(initId, {
+					'metadata': {
+						'client_id': client_id
+					}
+				});
+				
+				client.resetUser(initId);
+
+				var role = client.getFirstEntityWithRole(client.getMessagePart(), 'role');
+
+				if (role) {
+					client.updateConversationState({
+						requstedRole: role,
+					});
+
+					client.addTextResponse("Ok, " + forename + ", I'll check on your " + role.value);
+
+					client.done();
+
+				}
+
+
+
+			});
+
+
+
+
+
+		},
+
+
+		prompt: function () {
+
+			console.log("This means it can't identidy a role");
+
+			var tutorData = {
+				person: "wrong",
+				role: "wrong again"
+			};
+
+			client.addTextResponse("I'm not sure what's going on ... ");
+			client.addResponse('prompt_role');
+			// client.expect('provideAdvisor', ['clarify_role']);
+			client.done();
+		},
+
+	});
+
+
+	// was provideWeather
+	var provideAdvisor = client.createStep({
+		satisfied: function () {
+
+			console.log("provideAdvisor / satisfied");
+			return false;
+		},
+
+
+		prompt: function (eventType, payload, data) {
+			// Need to provide weather
+			console.log("Return data to provide_advisor");
+
+
+			var tutorData = {
+				person: "Joe Bloggs",
+				role: client.getFirstEntityWithRole(client.getMessagePart(), 'role').value
+			};
+
+			var advisor = people.filter(function (person) {
+
+				var messagePart = client.getMessagePart();
+				
+				console.log("init.ai n2:");
+				console.log(JSON.stringify(messagePart));
+				
+				var client_id = messagePart.sender.metadata.client_id;
+				
+				if (person.id === client.getMessagePart().sender.metadata.client_id) {
+
+					return person.advisor;
+
+				}
+
+			});
+
+
+			client.addTextResponse("looded up advisor: " + advisor);
+
+			var users = client.getUsers();
+
+			console.log(JSON.stringify(users));
+
+			client.addResponse('provide_advisor', tutorData);
+			client.done();
+
+
+
+
+		}
+	});
+
+
+	var handleEvent = function (eventType, payload) {
+			client.addTextResponse('Received event of type: ' + eventType);
+
+			var payloadText = JSON.stringify(payload);
+
+			client.addTextResponse('payload ' + payloadText);
+
+			client.done();
+		};
+
+
+	client.runFlow({
+		eventHandlers: {
+			'*': handleEvent
+		},
+		classifications: {
+			'request_advisor': 'getAdvisor'
+		},
+		streams: {
+			main: 'getAdvisor',
+			getAdvisor: [collectRole, provideAdvisor],
+			provideAdvisor: provideAdvisor
+		}
+	});
+
+
+};
+
+
+
+
+
+var people = [{
+	"id": "auth0|5815cb10344073a30129f746",
+	"advisor": "Mark Smith"
+}];
