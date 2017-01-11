7d544afb78a9abb04e30277cc762c588adc93c9d
Darryn Mitussis <darryn@imax.mitussis.net>
2017-01-11 21:36:35 +0000

    copy data from v1

---
+++ behavior/scripts/lib/getSmoochData.js
@@ -0,0 +1,41 @@
+
+var request = require('request');
+
+
+var smoochAPI = {
+	headers: {
+		'app-token': '9oe7vz42bik4rymee8cd8zgzq'
+	}
+};
+
+
+
+module.exports = function getCurrentWeather(smoochId, callback) {
+
+			smoochId = smoochId || "aaff1b14c18fb2e2d8ebb1d5";
+
+			smoochAPI.url = 'https://api.smooch.io/v1/appusers/${smoochId}';
+			
+			request.get(smoochAPI, function (error, response, body) {
+
+				if (error) {
+					console.log("smooch.io error:");
+					console.log(error);
+				}
+				// console.log(response);
+				// console.log(util.inspect(JSON.parse(body), false, null));
+				console.log("smooch.io:");
+				body = JSON.parse(body);
+
+				console.log(body);
+
+				var clientData = {forename: body.appUser.givenName, surname: body.appUser.surname, client_id: body.appUser.properties.id};
+				
+				callback(clientData);
+
+				
+
+			});
+
+
+};
